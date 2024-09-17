0748729e90a993db5c0f9eec880e089461620ffb
UratileMei <ST10105335@rcconnect.edu.za>
2024-09-17 13:22:43 +0000

    Login Layout Activity file added.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,14 @@ dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
+    //new implementation
+    implementation ("com.google.android.gms:play-services-maps:19.0.0")
+    implementation ("androidx.room:room-runtime:2.6.1")
+    annotationProcessor ("androidx.room:room-compiler:2.6.1")
+    implementation ("androidx.room:room-ktx:2.6.1")
+    implementation ("com.google.android.gms:play-services-location:21.3.0")
+    //end of new implementation
+    //
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
